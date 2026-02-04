ee9c0aef8bf95a782bfd1bc64d3f023a96d5760e
kittikhun.k <kittikhunk@fpt.com>
2026-02-04 10:01:29 +0700

    feat: add firebase env

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -9,7 +10,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.testintegrateui"
+        applicationId = "com.sabuywash.sabuywashapp"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -53,7 +54,7 @@ android {
         }
         create("dev") {
             dimension = "environment"
-            applicationIdSuffix = ".dev"
+            applicationIdSuffix = ".development"
             versionNameSuffix = "-dev"
         }
         create("uat") {
